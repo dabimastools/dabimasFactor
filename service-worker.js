6bd9c62ff1884aecd62e434b6057a97ee5a40fa0
dabimastools <90092604+dabimastools@users.noreply.github.com>
2025-01-19 05:28:59 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,87 @@
 // cache name, cache files
-var CACHE_NAME = 'dabimas-factor-v1';
+var CACHE_NAME = 'dabimas-factor-v20250119-01';
 var urlsToCache = [
-    '/yanaifarm.github.io/dabimasFactor/index.html',
-    '/yanaifarm.github.io/dabimasFactor/json/dabimasFactor.json',
-    '/yanaifarm.github.io/dabimasFactor/css/style.css',
-    '/yanaifarm.github.io/dabimasFactor/css/vuetify.min.css',
-    '/yanaifarm.github.io/dabimasFactor/css/vuetify_compact.min.css',
+  '/dabimastools.github.io/dabimasFactor/index.html',
+  '/dabimastools.github.io/dabimasFactor/json/dabimasFactor.json',
+  '/dabimastools.github.io/dabimasFactor/json/brosData.json',
+  '/dabimastools.github.io/dabimasFactor/css/style.css',
+  '/dabimastools.github.io/dabimasFactor/css/loading.css',
+  '/dabimastools.github.io/dabimasFactor/css/materialdesignicons.min.css',
+  '/dabimastools.github.io/dabimasFactor/css/materialdesignicons.min.css.map',
+  '/dabimastools.github.io/dabimasFactor/css/notosansjapanese.css',
+  '/dabimastools.github.io/dabimasFactor/css/vuetify_compact.min.css',
+  '/dabimastools.github.io/dabimasFactor/css/vuetify.min.css',
+  '/dabimastools.github.io/dabimasFactor/vue/vue.min.js',
+  '/dabimastools.github.io/dabimasFactor/vue/vuetify.js',
+  '/dabimastools.github.io/dabimasFactor/vue/vuetify.js.map',
+  '/dabimastools.github.io/dabimasFactor/fonts/materialdesignicons-webfont.eot',
+  '/dabimastools.github.io/dabimasFactor/fonts/materialdesignicons-webfont.ttf',
+  '/dabimastools.github.io/dabimasFactor/fonts/materialdesignicons-webfont.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/materialdesignicons-webfont.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Black.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Black.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Black.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Bold.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Bold.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Bold.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-DemiLight.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-DemiLight.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-DemiLight.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Light.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Light.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Light.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Medium.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Medium.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Medium.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Regular.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Regular.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Regular.woff2',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Thin.otf',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Thin.woff',
+  '/dabimastools.github.io/dabimasFactor/fonts/NotoSansJP-Thin.woff2',
 ];
 
 // install cache
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches
-        .open(CACHE_NAME)
-        .then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches
+    .open(CACHE_NAME)
+    .then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // use cache
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function (response) {
-                    return caches.open(CACHE_NAME).then(function (cache) {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-                });
-            });
-        })
-    );
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (response) {
+          return caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
 
 // refresh cache
 self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName !== CACHE_NAME;
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        }).then(function () {
-            clients.claim();
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
         })
-    );
+      );
+    }).then(function () {
+      clients.claim();
+    })
+  );
 });
 
