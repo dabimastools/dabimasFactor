34184fba01147fb5ec41ebf5b492c17d29701bb5
dabimastools <90092604+dabimastools@users.noreply.github.com>
2025-10-03 22:46:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // cache name, cache files
-var CACHE_NAME = 'dabimas-factor-v20250925-01';
+var CACHE_NAME = 'dabimas-factor-v20251003-01';
 var urlsToCache = [
   '/dabimastools.github.io/dabimasFactor/index.html',
   '/dabimastools.github.io/dabimasFactor/json/dabimasFactor.json',
