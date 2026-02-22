6cbe401a489337fd3e31eec7d5e775dfdce0cf74
yanaiFarm <nishiyan_pop@yahoo.co.jp>
2026-02-22 21:32:17 +0900

    データ不備を修正

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // cache name, cache files
-var CACHE_NAME = 'dabimas-factor-v20260222-01';
+var CACHE_NAME = 'dabimas-factor-v20260222-02';
 var BASE_PATH = self.location.pathname.replace(/\/service-worker\.js$/, '/');
 var urlsToCache = [
   BASE_PATH + 'index.html',
