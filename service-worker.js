4e4e0a6b570299790d9ac6afac2e29ed8ee9383c
dabimastools <nishiyan_pop@yahoo.co.jp>
2026-03-10 17:12:19 +0900

    Revert main to cb9738aaf48b317f976fd097027baa6ef8f055d2

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
 // cache name, cache files
-var CACHE_NAME = 'dabimas-factor-v20260310-02';
+var CACHE_NAME = 'dabimas-factor-v20260308-01';
 var BASE_PATH = self.location.pathname.replace(/\/service-worker\.js$/, '/');
-var APP_SHELL_URL = BASE_PATH + 'index.html';
 var urlsToCache = [
-  APP_SHELL_URL,
+  BASE_PATH + 'index.html',
   BASE_PATH + 'json/dabimasFactor.json',
   BASE_PATH + 'json/brosData.json',
   BASE_PATH + 'json/inbreed-exceptions.json',
@@ -44,101 +43,31 @@ var urlsToCache = [
   BASE_PATH + 'fonts/NotoSansJP-Thin.woff2',
 ];
 
-function isSameOrigin(url) {
-  return url.origin === self.location.origin;
-}
-
-function isOffline() {
-  return self.navigator && self.navigator.onLine === false;
-}
-
-function isCacheableResponse(response) {
-  return response && response.ok;
-}
-
-function putInCache(cacheKey, response) {
-  if (!isCacheableResponse(response)) {
-    return Promise.resolve(response);
-  }
-
-  return caches.open(CACHE_NAME).then(function (cache) {
-    cache.put(cacheKey, response.clone());
-    return response;
-  });
-}
-
-function fetchAndCache(request, cacheKey) {
-  return fetch(request).then(function (response) {
-    return putInCache(cacheKey || request, response);
-  });
-}
-
-function handleNavigationRequest(event) {
-  if (isOffline()) {
-    return caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || caches.match(APP_SHELL_URL) || Response.error();
-    });
-  }
-
-  return fetchAndCache(event.request, APP_SHELL_URL).catch(function () {
-    return caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || caches.match(APP_SHELL_URL) || Response.error();
-    });
-  });
-}
-
-function handleAssetRequest(event) {
-  return caches.match(event.request).then(function (cachedResponse) {
-    if (isOffline()) {
-      return cachedResponse || Response.error();
-    }
-
-    var networkResponsePromise = fetchAndCache(event.request).catch(function () {
-      return cachedResponse;
-    });
-
-    if (cachedResponse) {
-      event.waitUntil(networkResponsePromise.then(function () {
-        return undefined;
-      }));
-      return cachedResponse;
-    }
-
-    return networkResponsePromise;
-  });
-}
-
 // install cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches
-      .open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      })
-      .then(function () {
-        return self.skipWaiting();
-      })
+    .open(CACHE_NAME)
+    .then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // use cache
 self.addEventListener('fetch', function (event) {
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
-  var requestUrl = new URL(event.request.url);
-  if (!isSameOrigin(requestUrl) || requestUrl.searchParams.has('app-check')) {
-    return;
-  }
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(handleNavigationRequest(event));
-    return;
-  }
-
-  event.respondWith(handleAssetRequest(event));
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (response) {
+          return caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
 
 // refresh cache
@@ -153,7 +82,7 @@ self.addEventListener('activate', function (event) {
         })
       );
     }).then(function () {
-      return self.clients.claim();
+      clients.claim();
     })
   );
 });
